ad6581c4e020d655c37d17d677896cea2b712e5b
jovace <jorge.vazquez.acevedo@alumnos.upm.es>
2019-11-05 15:40:09 +0100

    Configurar logger

--- src/main/java/ingsoft1920/impldemo/ImpldemoApplication.java
+++ src/main/java/ingsoft1920/impldemo/ImpldemoApplication.java
@@ -3,10 +3,15 @@ package ingsoft1920.impldemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class ImpldemoApplication {
+	final static Logger logger = LogManager.getLogger(ImpldemoApplication.class.getName());
 
 	public static void main(String[] args) {
+		logger.info("Aplicacion iniciada");
 		SpringApplication.run(ImpldemoApplication.class, args);
 	}
 
