7e947a825dc2641bb6718c739048bc06a54ce596
jovace <rmjuw21@gmail.com>
2019-11-05 12:54:58 +0100

    Commit inicial

---
+++ src/test/java/ingsoft1920/impldemo/ImpldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoft1920.impldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
